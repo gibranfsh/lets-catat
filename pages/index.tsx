2a5a11878d72d4a8ab3d2e5bc5371592fe945d6f
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2023-06-07 23:59:54 +0700

    init

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="mt-10">
+      <h1>Welcome to the Homepage</h1>
+      <p>This is the main page of your Next.js application.</p>
+    </div>
+  );
+};
+
+export default HomePage;
